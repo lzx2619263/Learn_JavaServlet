a9cc6315601476c7074c75b77c8c4c8bba08bec4
lzx2619263 <lzx2619263@163.com>
2019-05-03 09:04:41 +0800

    commit

---
+++ src/main/java/learn/java/servlet/Learn_JavaServlet.java
@@ -0,0 +1,52 @@
+package learn.java.servlet;
+
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class Learn_JavaServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String newUrl = System.getProperty("sysenv.NEWURL");
+		String getBackString = this.sendGet(newUrl);
+		response.getWriter().write(getBackString);
+
+		
+	}
+	
+	//使用httpclient发送get请求
+	protected String sendGet(String url) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet request = new HttpGet(url);
+		CloseableHttpResponse response = null;
+		String resultString = null;
+		try {
+			response = httpClient.execute(request);
+			HttpEntity entity = response.getEntity();
+			if(entity != null) {
+				resultString = EntityUtils.toString(entity,"UTF-8").trim();
+			}
+			response.close();
+			httpClient.close();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return resultString;
+	}
+
+}
