83a9a613823ec7acf7940d2627ce168b99391ad7
lzx2619263 <lzx2619263@163.com>
2019-05-09 14:00:58 +0800

    commit

--- src/main/java/learn/java/servlet/Learn_JavaServlet.java
+++ src/main/java/learn/java/servlet/Learn_JavaServlet.java
@@ -1,7 +1,10 @@
 package learn.java.servlet;
 
 import java.awt.List;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,35 +27,59 @@ public class Learn_JavaServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//获取环境变量
-//		Map<String, String> envMap = new HashMap<String, String>();
-//		envMap = System.getenv();
-		
 		Map<String, String> envMap = System.getenv();
-		ArrayList<String> nextServer = new ArrayList<String>();
-		
+		ArrayList<String> nextHttpServer = new ArrayList<String>();
+		ArrayList<String> nextTcpServer = new ArrayList<String>();
 		
 		//获取本机服务名称
 		String hostName = envMap.get("HOSTNAME").split("-")[0];
 		String respString = "==>" + hostName;;
 		
-		//获取连线组件信息(连线的环境变量必须满足NEXTSERVER+数字样式)
+		//获取HTTP连线组件信息(连线的环境变量必须满足NEXTSERVER+数字样式)
 		for(String key : envMap.keySet()) {
 			if(key.contains("NEXTSERVER")) {
-				nextServer.add(envMap.get(key).toUpperCase());
+				nextHttpServer.add(envMap.get(key).toUpperCase());
 			}
 		}
 		
-		//如果环境变量中不存在其他连线组件，则返回服务名称结果
-		if(nextServer.isEmpty()) {
-			response.getWriter().write(respString);
-		}else {
-			for(String next:nextServer) {
-				String nextHost = envMap.get(next + "_SERVICE_HOST");
-				String nextPort = envMap.get(next + "_SERVICE_PORT");
-				String nextUrl = "http://" + nextHost + ":" + nextPort;
-				String nexResp = sendGet(nextUrl, next.toLowerCase());
-				respString = respString + nexResp;
+		//获取TCP连线组件信息(连线的环境变量必须满足TCPSERVER+数字样式)
+		for(String key : envMap.keySet()) {
+			if(key.contains("TCPSERVER")) {
+				nextTcpServer.add(envMap.get(key).toUpperCase());
 			}
+		}
+		
+		//如果环境变量中不存在其他HTTP或TCP连线组件，则返回服务名称结果
+//		if(nextHttpServer.isEmpty() && nextTcpServer.isEmpty()) {
+//			response.getWriter().write(respString);
+//		}else {
+//			for(String next:nextHttpServer) {
+//				String nextHost = envMap.get(next + "_SERVICE_HOST");
+//				String nextPort = envMap.get(next + "_SERVICE_PORT");
+//				String nextUrl = "http://" + nextHost + ":" + nextPort;
+//				String nexResp = sendGet(nextUrl, next.toLowerCase());
+//				respString = respString + nexResp;
+//			}
+//			response.getWriter().write(respString);
+//		}
+		
+		if(!nextHttpServer.isEmpty()) {
+			for(String nextHttp:nextHttpServer) {
+				String nextHttpHost = envMap.get(nextHttp + "_SERVICE_HOST");
+				String nextHttpPort = envMap.get(nextHttp + "_SERVICE_PORT");
+				String nextHttpUrl = "http://" + nextHttpHost + ":" + nextHttpPort;
+				String nextHttpResp = httpGetRequest(nextHttpUrl, nextHttp.toLowerCase());
+				respString = respString + nextHttpResp;
+			}
+		}else if (!nextTcpServer.isEmpty()) {
+			for(String nextTcp:nextTcpServer) {
+				String nextTcpHost = envMap.get(nextTcp + "_SERVICE_HOST");
+				int nextTcpPort = Integer.valueOf(envMap.get(nextTcp + "_SERVICE_PORT"));
+//				String nextTcpUrl = "http://" + nextTcpHost + ":" + nextTcpPort;
+				String nextTcpResp = TcpRequest(nextTcpHost, nextTcpPort, nextTcp.toLowerCase());
+				respString = respString + nextTcpResp;
+			}
+		}else {
 			response.getWriter().write(respString);
 		}
 		
@@ -60,7 +87,7 @@ public class Learn_JavaServlet extends HttpServlet {
 	}
 	
 	//使用httpclient发送get请求到指定url
-	protected String sendGet(String url, String serverName) {
+	protected String httpGetRequest(String url, String serverName) {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpGet request = new HttpGet(url);
 		CloseableHttpResponse response = null;
@@ -79,6 +106,24 @@ public class Learn_JavaServlet extends HttpServlet {
 		} catch (IOException e) {
 			e.printStackTrace();
 			resultString = serverName + " Exception";
+		}
+		return resultString;
+	}
+	
+	
+	//发送TCP请求
+	protected String TcpRequest(String host, int port, String serverName) {
+		String resultString = null;
+		try {
+			Socket s = new Socket(host,port);
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			resultString = "==>" + dis.readUTF();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			resultString = "==>" + serverName + "Exception";
+		} catch (IOException e) {
+			e.printStackTrace();
+			resultString = "==>" + serverName + "Exception";
 		}
 		return resultString;
 	}
